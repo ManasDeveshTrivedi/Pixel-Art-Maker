5d831b49070b6fd2ade369472f2156a53f3cbce0
Manas <manasdevtrivedi@gmail.com>
2026-04-12 21:01:47 +0530

    Added

---
+++ p10.js
@@ -0,0 +1,5 @@
+//Initial references
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
